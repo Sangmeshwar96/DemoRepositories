761d5d20c367e5fa7ad5855e4d9de673aa99e882
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-27 20:52:49 +0530

    [Sangmeshwar]:Array Demo Practice

---
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArryDemo
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            int[] my_array = new int[4];
+
+            my_array[0] = 11;
+            my_array[1] = 22;
+            my_array[2] = 33;
+            my_array[3] = 44;
+
+            //for (int i = 0; i < my_array.Length; i++)
+            //{
+            //    Console.WriteLine(my_array[i]);
+            //}
+
+            foreach (int item in my_array)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            ////Console.WriteLine(my_array[2]);
+
+
+        }
+    }
+}
