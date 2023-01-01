a0fd10ca73684111d5f3b42704960c518df681df
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2023-01-01 22:19:25 +0530

    [Sangmeshwar]:Queue Using Generics Demo Practice

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackQueueDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            QueueClass<int> queueClass = new QueueClass<int>();
+            queueClass.Enqueue(56);
+            queueClass.Enqueue(30);
+            queueClass.Enqueue(70);
+            queueClass.Display();
+            queueClass.DeQueueAll();
+            queueClass.Display();
+
+
+        }
+    }
+}
---
+++ Queue/Queue/QueueClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueueDemo
+{
+    class QueueClass<T>
+    {
+        Node<T> head;
+   
+        public void Enqueue(T item)
+        {
+         
+            Node<T> new_node = new Node<T>(item);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+           
+            else
+            {
+                Node<T> temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+            }
+            Console.WriteLine("inserted into list " + new_node.data);
+        }
+     
+        public void Dequeue()
+        {
+           
+            if (this.head == null)
+            {
+                Console.WriteLine("The Queue is empty");
+                return;
+            }
+
+          
+            Node<T> temp = this.head;
+            this.head = this.head.next;
+
+            Console.WriteLine("\nItem deleted is {0}", temp.data);
+        }
+
+        public void DeQueueAll()
+        {
+
+            while (head != null)
+                Dequeue();
+        }
+   
+        public void Display()
+        {
+            Node<T> temp = this.head;
+       
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+           
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
